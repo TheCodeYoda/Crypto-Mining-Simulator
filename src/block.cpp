c77d452a86d559e09b0f59198974e44df05bef40
TheCodeYoda <smurf61988@gmail.com>
2021-09-09 19:51:25 +0530

    block structure and basic definition completed

---
+++ src/block.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "block.hpp"
+#include "sha256.hpp"
+
+Block::Block(string data, string previous_block_hash)
+{
+  this->data = data;
+  this->prev_block_hash = previous_block_hash;
+  this->hash = this->compute_valid_hash(data, previous_block_hash);
+}
+
+/* checks whether hash starts with 3 zeroes */
+bool Block::is_hash_valid(string hash)
+{
+  if (hash[0] == '0' && hash[1] == '0' && hash[2] == '0') {
+    return true;
+  }
+  return false;
+}
+
+/* calculates valid hash for the block (similar to mining)*/
+string Block::compute_valid_hash(string data, string previous_block_hash)
+{
+  string hash = "";
+  int nonce = 0;
+
+  while (!is_hash_valid(hash)) {
+    string input_string = data + "\t" + previous_block_hash + to_string(nonce);
+    hash = sha256(input_string);
+    ++nonce;
+  }
+
+  return hash;
+}
+
+void Block::display()
+{
+  cout << "data: " << this->data << endl;
+  cout << "previous block hash: " << this->prev_block_hash << endl;
+  cout << "current block hash: " << this->hash << endl;
+  cout << endl;
+}
---
+++ src/block.hpp
@@ -0,0 +1,18 @@
+#ifndef BLOCK_H
+#define BLOCK_H
+
+#include <string>
+
+class Block {
+ private:
+  std::string data;
+  std::string prev_block_hash;
+  std::string hash;
+  std::string compute_valid_hash(std::string, std::string);
+  bool is_hash_valid(std::string hash);
+
+ public:
+  Block(std::string data, std::string previous_block_hash);
+  void display();
+};
+#endif
