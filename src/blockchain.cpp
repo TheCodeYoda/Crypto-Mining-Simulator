7346164e2efdf82f1e6469b1dc65adf4e6d249da
TheCodeYoda <smurf61988@gmail.com>
2021-11-09 15:44:26 +0530

    added new function add_transaction()

--- src/blockchain.cpp
+++ src/blockchain.cpp
@@ -8,24 +8,40 @@ using namespace std;
 Blockchain::Blockchain()
 {
   this->difficulty = 3;
-  this->set_genesis_block();
 }
 
-/* constructs blockchain with genesis block */
+/* constructs blockchain with given difficulty*/
 Blockchain::Blockchain(int difficulty)
 {
   this->difficulty = difficulty;
-  this->set_genesis_block();
+}
+
+/* add transaction to the blockchain */
+
+void Blockchain::add_transaction(string transaction, int miner_id)
+{
+  this->mpool.add_transaction_to_pool(transaction);
+  /* create block if mpool has 5 transactions */
+  if (this->mpool.size() == 5) {
+    vector<string> slice(mpool.transaction_pool.begin(), mpool.transaction_pool.end());
+    if (this->blocks.size() == 0) {
+      this->set_genesis_block(slice);
+    }
+    else {
+      this->add_block(slice, miner_id);
+    }
+    this->mpool.clear();
+  }
 }
 
 /* sets the very first block with prev hash being null hash */
-void Blockchain::set_genesis_block()
+void Blockchain::set_genesis_block(vector<string> transaction_slice)
 {
   string null_hash = "";
   for (int i = 0; i < 64; i++) {
     null_hash += '0';
   }
-  Block genesis_block = Block("genesis", null_hash, this->difficulty, -1);
+  Block genesis_block = Block(transaction_slice, null_hash, this->difficulty, -1);
   this->blocks.push_back(genesis_block);
 }
 
@@ -37,16 +53,19 @@ string Blockchain::give_last_hash()
 }
 
 /* adds block to the blockchain */
-void Blockchain::add_block(string data, int miner_id)
+void Blockchain::add_block(vector<string> transactions, int miner_id)
 {
   /* mines for a new block */
-  Block new_block = Block(data, this->give_last_hash(), this->difficulty, miner_id);
+  Block new_block = Block(transactions, this->give_last_hash(), this->difficulty, miner_id);
   this->blocks.push_back(new_block);
 }
 
 /* validates blockchain */
 bool Blockchain::validate_blockchain()
 {
+  if (!this->blocks.size()) {
+    return true;
+  }
   Block prev = blocks[0];
   for (int i = 1; i < blocks.size(); i++) {
     Block curr = blocks[i];
--- src/blockchain.hpp
+++ src/blockchain.hpp
@@ -4,18 +4,21 @@
 using namespace std;
 
 #include "block.hpp"
+#include "mempool.hpp"
 
 class Blockchain {
  private:
   vector<Block> blocks;
+  Mempool mpool;
   int difficulty;
-  void set_genesis_block();
+  void set_genesis_block(vector<string> transaction_slice);
   string give_last_hash();
 
  public:
   Blockchain();
   Blockchain(int difficulty);
-  void add_block(string data, int miner_id);
+  void add_block(vector<string> transactions, int miner_id);
+  void add_transaction(string transaction, int miner_id);
   bool validate_blockchain();
   void display();
 };
